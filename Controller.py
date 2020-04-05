f5dd53be2b13c554998146a17c7614d6b57039fb
raul <raulrs14@gmail.com>
2020-04-05 13:53:56 +0200

    Car distance improved but under revision

--- Controller.py
+++ Controller.py
@@ -15,44 +15,48 @@ class Controller():
 
         while not wall:
             x0, x1, y0, y1 = 0e0, 1e0, 0e0, 1e0
-           # x0, x1, y0, y1 = my_circuit.get_block_coor(block)
+            # x0, x1, y0, y1 = my_circuit.get_block_coor(block)
+            xlim=x0
+            ylim=y0
             if np.cos(angle) >= 0e0:
-                x0 = x1
+                xlim = x1
             if np.sin(angle) >= 0e0:
-                y0 = y1
+                ylim = y1
 
-            x = car.get_coor()[0]
-            y = car.get_coor()[1]
+            x = xlim 
+            y = ylim
 
-            if np.sin(angle) != 0e0:
+            if np.abs(np.sin(angle)) > 1e-5:
                 x = np.cos(angle)/np.sin(angle)*\
-                        (x0 - car.get_coor()[0]) + car.get_coor()[1]
+                        (ylim - car.get_coor()[1]) + car.get_coor()[0]
             else:
-                x = x0
+                x = xlim
+                y = car.get_coor()[1]
     
             if x0 <= x <= x1:
                 wall = True
                # wall = my_circuit.is_wall(block,x,y0)
     
             else:
-                if np.cos(angle) != 0e0:
+                x = xlim 
+                y = ylim
+                if np.abs(np.cos(angle)) > 1e-5:
                     y = np.sin(angle)/np.cos(angle)*\
-                            (y0 - car.get_coor()[1]) + car.get_coor()[0]
+                            (xlim - car.get_coor()[0]) + car.get_coor()[1]
                 else:
-                    y = y0
+                    y = car.get_coor[1]
 
                 wall = True
                # wall = my_circuit.is_wall(x0,y)
 
             block += 1
             #if block ==  my_circuit.nblocks: block = 0
-
-        print(x," ",car.get_coor()[0]," ",y," ",car.get_coor()[1])
         dist = np.sqrt((x-car.get_coor()[0])**2+(y-car.get_coor()[1])**2)
         return dist
+
+
     
      
 
 main_controller = Controller()
 car1 = Car()
-print(main_controller.car_dist(car1,0e0))
